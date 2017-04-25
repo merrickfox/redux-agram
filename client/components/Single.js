5874b567dcbf19c574d803933ea5650c8d13f959
Merrick Fox <merrick.fox@gmail.com>
2017-04-25 16:56:26 +0100

    add router and single and grid components

---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        I'm single
+      </div>
+    )
+  }
+})
+
+
+export default Single;
